b0c3794780fa4c1f9b15ca01d85eeccd8624ca11
Alok Kumar Pandey <alokumarpandey76@gmail.com>
2026-06-22 12:35:12 +0530

    13-ai-module-part-2

--- modules/playground/components/toggle-ai.tsx
+++ modules/playground/components/toggle-ai.tsx
@@ -35,6 +35,7 @@ import {
 } from "lucide-react";
 import React from "react";
 import { cn } from "@/lib/utils";
+import { AIChatSidePanel } from "@/modules/ai-chat/components/ai-chat-sidebarpanel";
 // import { AIChatSidePanel } from "@/features/ai-chat/components/ai-chat-sidepanel";
 
 
@@ -191,6 +192,10 @@ const ToggleAI: React.FC<ToggleAIProps> = ({
           </DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenu>
+      <AIChatSidePanel
+      isOpen={isChatOpen}
+      onClose={() => setIsChatOpen(false)}
+      />
 
     </>
   );
